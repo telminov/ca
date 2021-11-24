0bda9667e7dbc11f0b253b5db6959df91ec5605d
telminov <telminov@soft-way.biz>
2021-11-24 21:59:10 +0300

    database path from env

--- project/settings.py
+++ project/settings.py
@@ -90,7 +90,7 @@ WSGI_APPLICATION = 'project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.getenv('DB_PATH', os.path.join(BASE_DIR, 'db.sqlite3')),
     }
 }
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
